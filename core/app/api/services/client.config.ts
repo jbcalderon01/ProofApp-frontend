2f13c2015a598eb61f6dfdf44c94b7611fa16301
Johnny Bernal <bernaljohnny3@gmail.com>
2022-04-11 09:06:28 -0500

    m

--- core/app/api/services/client.config.ts
+++ core/app/api/services/client.config.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 // Axios Client
 export const AxiosClient = axios.create({
-    // baseURL: 'https://api-proof-app.herokuapp.com/',
+    baseURL: 'https://api-proof-app.herokuapp.com/',
     timeout: 1000
 })
 
